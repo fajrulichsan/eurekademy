0bc9367b8372853e77bdd776b5a8cf35f380dc0a
fajrulichsan <fajrul.ichsankamil@ai.astra.co.id>
2023-08-19 00:24:01 +0700

    add send email signUp

--- src/controllers/UserControllers.js
+++ src/controllers/UserControllers.js
@@ -1,5 +1,6 @@
-// import User from '../models/UserModels.js'
 const sequelize = require("../config/database")
+const { v4: uuidv4 } = require('uuid');
+const nodemailer = require('nodemailer');
 const User = require("../models/UserModels")
 
 exports.getAllUser = async (req, res) => {
@@ -26,12 +27,57 @@ exports.getUserById = async (req, res) =>{
 
 exports.createUser = async (req, res) =>{
     try{
-        req.body.id = new Date().getTime();
-        await User.create(req.body)
-        res.status(201).json({msg : 'Success create user'}); 
+        const user = {
+            id: new Date().getTime(),
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+            token: uuidv4(),
+        };
+
+        const name = String(user.name).trim();
+        const email = String(user.email).trim();
+        const password = String(user.password);
+
+        if (name.length === 0) {
+            return res.status(400).json({ acknowledge: false ,message: "Name tidak boleh kosong" });
+        }
+
+        if (email.length === 0) {
+            return res.status(400).json({acknowledge: false, message: "Email tidak boleh kosong" });
+        } else if (!isValidEmail(email)) {
+            return res.status(400).json({ acknowledge: false,  message: "Email tidak valid" });
+        }
+
+        if (password.length === 0) {
+            return res.status(400).json({acknowledge: false,  message: "Password tidak boleh kosong" });
+        } else if (password.length < 8) {
+            return res.status(400).json({acknowledge: false,  message: "Password harus memiliki setidaknya 8 karakter" });
+        }
+
+        const createdUser = await User.create(user);
+        if (createdUser) {
+            // Kirim email verifikasi
+            const verificationLink = `http://localhost:5173/verify/${user.token}`; // Ganti dengan base URL Anda
+            const mailOptions = {
+                from: 'payogot@gmail.com',
+                to: user.email,
+                subject: 'Email Verification',
+                text: `Click this link to verify your email: ${verificationLink}`,
+            };
+
+            try {
+                await transporter.sendMail(mailOptions);
+                console.log('Verification email sent to:', user.email);
+            } catch (error) {
+                console.error('Error sending verification email:', error);
+            }
+        }
+        return res.status(201).json({acknowledge: true, message: 'Success create user' });
+
     }catch (error){
         console.error('Error fetching users:', error);
-        res.status(500).json({ message: error.errors });
+        res.status(500).json({acknowledge: false,  message: error.errors[0].message });
     }
 }
 
@@ -63,3 +109,16 @@ exports.deleteUser = async (req, res) =>{
         res.status(500).json({ message: 'Internal server error' });
     }
 }
+
+const isValidEmail = (email) => {
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailPattern.test(email);
+}
+
+const transporter = nodemailer.createTransport({
+  service: 'Gmail',
+  auth: {
+    user: 'payogot@gmail.com',
+    pass: 'fmzxtxxurjeymjzg',
+  },
+});
