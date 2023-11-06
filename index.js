14ddfc64d83bc346e14ef5ab1392e11f906afa07
psmfajru1107 <fajrul.ichsankamil@ai.astra.co.id>
2023-11-06 14:45:42 +0700

    fix: init project

--- index.js
+++ index.js
@@ -6,13 +6,17 @@ const UserRoute = require("./src/routers/UserRoute")
 const app = express()
 
 // app.use(cors)
-app.use(cors({
-  origin: 'http://localhost:5173',
-  optionsSuccessStatus: 200 // Beberapa browser membutuhkan ini untuk mengizinkan metode HTTP lain selain GET/POST
-}));
+// app.use(cors({
+//   origin: 'http://localhost:5173',
+//   optionsSuccessStatus: 200 // Beberapa browser membutuhkan ini untuk mengizinkan metode HTTP lain selain GET/POST
+// }));
 
-app.use(express.json())
-app.use("/", UserRoute);
+// app.use(express.json())
+// app.use("/", UserRoute);
+
+app.get("/", (res, req) => {
+  res.set("Welcome")
+})
 
 
 app.listen(3000, () => console.log("running server"))
